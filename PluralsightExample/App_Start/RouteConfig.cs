0c4d913d113485f79f58207f41df8ac6f22709e1
Pedro <pedro.cheroto@gmail.com>
2017-05-24 23:35:02 -0400

    playing with MVC 2

--- PluralsightExample/App_Start/RouteConfig.cs
+++ PluralsightExample/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace PluralsightExample
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name:"test",
+                url: "test/{id}",
+                defaults: new {controller = "Test", action = "Index", id = ""}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- PluralsightExample/Controllers/HomeController.cs
+++ PluralsightExample/Controllers/HomeController.cs
@@ -11,6 +11,12 @@ namespace PluralsightExample.Controllers
     {
         public ActionResult Index()
         {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            ViewBag.Message = string.Format("{0}::{1} {2}", controller, action, id);
+
             return View();
         }
 
